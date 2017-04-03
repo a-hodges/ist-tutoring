f1a70c9a0a39dec44727e24c4c4be043f1607dfb
brian-hodges <brjhodges@gmail.com>
2017-04-03 17:40:00 -0500

    Fixed course picker
    Now allows pre-selection of a course section in the HTML

--- static/js/course_picker.js
+++ static/js/course_picker.js
@@ -1,9 +1,18 @@
 let options;
 function get_sections(){
     let course = $('#course_id').val();
-    $('#section_id').val('');
-    $('#section_id optgroup').remove();
-    $('#section_id').append(options.filter('[parent="{0}"]'.format(course)));
+    let remove_selector = '#section_id optgroup:not([parent="{0}"])'.format(course);
+    let remove = $(remove_selector);
+    if ($('#section_id optgroup').length == remove.length){
+        $('#section_id').val('');
+        remove.remove();
+        $('#section_id').append(
+            options.filter('[parent="{0}"]'.format(course))
+        );
+    }
+    else {
+        remove.remove();
+    }
 }
 
 $(function(){
