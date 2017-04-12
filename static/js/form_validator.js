4a56a60578e0cddd08a8a70eda117dd78bb79521
brian-hodges <brjhodges@gmail.com>
2017-04-11 21:16:41 -0500

    Moved the form validator to a separate script
    It actually worked! (somehow)

---
+++ static/js/form_validator.js
@@ -0,0 +1,15 @@
+function onSubmit(token){
+    let form = document.getElementById("form");
+    $(':valid').parent('.formgroup').removeClass('has-error');
+    $(':invalid').parent('.formgroup').addClass('has-error');
+    if (form.checkValidity()){
+        form.submit();
+    }
+    else {
+        $('.g-recaptcha')
+            .off('**')
+            .click(function(){onSubmit(token);})
+            .attr('name', 'g-recaptcha-response')
+            .attr('value', token);
+    }
+}
